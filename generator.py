ff94228b70fff9cbbb2b6620a8e9a46ca7c9f378
udonudon <zamasu@gmail.com>
2018-03-04 00:02:10 +0900

    multi threading

--- generator.py
+++ generator.py
@@ -3,6 +3,23 @@
 from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 from math import *
+import multiprocessing
+from multiprocessing import Process
+from multiprocessing import Pool
+
+# マルチプロセスの許可
+# pool or full or not が選べる
+multiprocess_mode = "pool"
+
+# 作成する画像数
+numOfImages=5
+
+# 消失点の表示
+showVanishingPoints = True
+
+# https://i.imgur.com/KguoTZ9.jpg
+image = Image.open('KguoTZ9.jpg')
+image = image.resize((int(image.size[0]/image.size[1]*400), 400))
 
 class Point:
     def dist(self,target):
@@ -36,7 +53,7 @@ def mat2img(mat, bf):
     bf.putdata(pxx)
     return
 
-def getCrossPoint(pixel,hoge,huge):
+def getCrossPoint(pixel,hoge,huge,to):
     vector_To2Pixel=pixel.minus(to)
 
     tmpx=huge.x-hoge.x
@@ -47,12 +64,8 @@ def getCrossPoint(pixel,hoge,huge):
         r=((tmpx*to.y - tmpy*to.x) - (tmpx*hoge.y - tmpy*hoge.x))/(0.00001)
     return to.add(vector_To2Pixel.mul(r)),r
 
-# https://i.imgur.com/KguoTZ9.jpg
-image = Image.open('KguoTZ9.jpg')
-image = image.resize((int(image.size[0]/image.size[1]*400), 400))
-numOfImages=50
-showVanishingPoints = True
-for i in range(numOfImages):
+
+def generate(i):
     i1 = Image.new("RGB", (image.size[0], image.size[1]), (120, 150, 200))
 
     mat = img2mat(image,3)
@@ -69,10 +82,10 @@ for i in range(numOfImages):
     for x in range(image.size[1]):
         for y in range(image.size[0]):
             pixel=Point(x,y)
-            cross1,r1=getCrossPoint(pixel,p1,p4)
-            cross2,r2=getCrossPoint(pixel,p2,p1)
-            cross3,r3=getCrossPoint(pixel,p3,p2)
-            cross4,r4=getCrossPoint(pixel,p4,p3)
+            cross1,r1=getCrossPoint(pixel,p1,p4,to)
+            cross2,r2=getCrossPoint(pixel,p2,p1,to)
+            cross3,r3=getCrossPoint(pixel,p3,p2,to)
+            cross4,r4=getCrossPoint(pixel,p4,p3,to)
             dist=1000000000000000
             crosses=[cross1,cross2,cross3,cross4]
             ratios=[r1,r2,r3,r4]
@@ -91,19 +104,28 @@ for i in range(numOfImages):
             if(showVanishingPoints):
                 mat[int(cross.x)][int(cross.y)]=(255,255,255)
     if(showVanishingPoints): # show vanishing points
-        mat[p1.x][p1.y]=(255,255,255)
-        mat[p2.x][p2.y]=(255,255,255)
-        mat[p3.x][p3.y]=(255,255,255)
-        mat[p4.x][p4.y]=(255,255,255)
-        mat[int(fr.x)][int(fr.y)]=(255,0,0) #red
-        mat[int(fr.x+1)][int(fr.y)]=(255,0,0) #red
-        mat[int(fr.x)][int(fr.y+1)]=(255,0,0) #red
-        mat[int(fr.x+1)][int(fr.y+1)]=(255,0,0) #red
-        mat[int(to.x)][int(to.y)]=(0,0,255) #blue
-        mat[int(to.x+1)][int(to.y)]=(0,0,255) #blue
-        mat[int(to.x)][int(to.y+1)]=(0,0,255) #blue
-        mat[int(to.x+1)][int(to.y+1)]=(0,0,255) #blue
+        def setcolor(pixel,color):
+            for j in [(0,0),(0,1),(1,0),(1,1)]:
+                mat[int(pixel.x)+j[0]][int(pixel.y)+j[1]]=color
+        # set color RED
+        setcolor(fr, (255,0,0))
+        # set color BLUE
+        setcolor(to, (0,255,255))
 
     mat2img(mat,i1)
-    # i1.show()
     i1.save(str(i).zfill(3) + ".png")
+
+if multiprocess_mode == "pool":
+    # プロセス数の最大値
+    pool = Pool(max(1, multiprocessing.cpu_count() - 1))
+    pool.map(generate, range(numOfImages))
+    pool.close
+else:
+    for i in range(numOfImages):
+        if multiprocess_mode == "full":
+            # pool非使用時はこちら
+            p = Process(target=generate,args=(i,))
+            p.start()
+        else:
+            # シリアルに実行する
+            generate(i)
